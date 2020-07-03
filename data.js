68700785e524b0a474c2bbb5196df9d40d1ae2ae
Azzam <azzam.rar@gmail.com>
2020-07-03 14:04:32 +0300

    added a new data file to store the node tree

---
+++ data.js
@@ -0,0 +1,26 @@
+let x = {
+  name: "x",
+  children: [
+    {
+      name: "a",
+      children: [],
+    },
+    {
+      name: "b",
+      children: [
+        {
+          name: "o",
+          children: [],
+        },
+      ],
+    },
+    {
+      name: "c",
+      children: [],
+    },
+    {
+      name: "d",
+      children: [],
+    },
+  ],
+};
