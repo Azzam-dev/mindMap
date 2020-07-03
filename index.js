48b22903122062828ea49f0386a4a7bf00cdebcb
Saleh althini <salehalthini@gmail.com>
2020-07-03 14:34:38 +0300

    add Three Functions in index.js For drawCircle and drawLine drawSquare

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
+let canvasCenter = canvas.width / 2;
+ctx.strokeStyle = "#00f";
 
-ctx.strokeStyle = "#f00";
 
-ctx.moveTo(0, 0);
-ctx.lineTo(100, 0);
-ctx.lineTo(100, 100);
-ctx.lineTo(0, 100);
-ctx.lineTo(0, 0);
 
-ctx.stroke();
+
+function drawCircle() {
+    ctx.beginPath();
+    ctx.arc(canvasCenter, 50, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+
+}
+
+function drawLine() {
+    ctx.moveTo(100, 100);
+    ctx.lineTo(200, 0);
+    ctx.stroke();
+}
+
+function drawSquare() {
+    ctx.moveTo(1, 0);
+    ctx.lineTo(100, 0);
+    ctx.lineTo(100, 100);
+    ctx.lineTo(0, 100);
+    ctx.lineTo(0, 0);
+    ctx.stroke();
+
+}
+
+
+drawCircle();
+drawLine();
