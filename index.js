559f8944aa853d222f4f3af8fe1dab67905a3e80
Faisal-At <fynth2@gmail.com>
2020-07-04 17:43:47 +0300

    added names for each node

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
-
 let canvasXCenter = canvas.width / 2;
 let canvasYCenter = canvas.height / 2;
 ctx.strokeStyle = "#00f";
-let radius = 45
+ctx.fillStyle = "#00f";
+ctx.textAlign = "center";
+let radius = 50
+ctx.font = `${radius / 2.5}px Arial`;
 let margin = radius * 1.1
 let level = radius * 2 + margin
-function drawCircle(x, y) {
+function drawCircle(x, y, name) {
   ctx.beginPath();
   ctx.arc(x, y, radius, 0, 2 * Math.PI);
   ctx.stroke();
+  ctx.fillText(name , x , y + 5 , 20)
 }
 
 function drawLine(x1, y1, x2, y2) {
@@ -27,9 +30,13 @@ drawLine(canvasXCenter, level * 1 + radius, canvasXCenter + margin * 3, level * 
 drawLine(canvasXCenter - margin , level * 2 + radius , canvasXCenter - margin, level * 3 - radius); //O
 
 
-drawCircle(canvasXCenter, level * 1); //X
-drawCircle(canvasXCenter - margin * 3 , level * 2) //A
-drawCircle(canvasXCenter - margin, level * 2); //B
-drawCircle(canvasXCenter + margin, level * 2); //C
-drawCircle(canvasXCenter + margin * 3, level * 2); //D
-drawCircle(canvasXCenter - margin, level * 3); //O
+drawCircle(canvasXCenter, level * 1 , "X"); //X
+drawCircle(canvasXCenter - margin * 3 , level * 2 , "A") //A
+drawCircle(canvasXCenter - margin, level * 2 , "B"); //B
+drawCircle(canvasXCenter + margin, level * 2 , "C"); //C
+drawCircle(canvasXCenter + margin * 3, level * 2 , "D"); //D
+drawCircle(canvasXCenter - margin, level * 3 , "O"); //O
+
+
+
+
