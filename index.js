3112e1ebab1e825aa636491b226fffbbc78e62ce
Faisal-At <fynth2@gmail.com>
2020-07-04 17:01:00 +0300

    Refactored the code and worked on improving code quality

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ let ctx = canvas.getContext("2d");
 let canvasXCenter = canvas.width / 2;
 let canvasYCenter = canvas.height / 2;
 ctx.strokeStyle = "#00f";
-
+let radius = 45
+let margin = radius * 1.1
+let level = radius * 2 + margin
 function drawCircle(x, y) {
   ctx.beginPath();
-  ctx.arc(x, y, 50, 0, 2 * Math.PI);
+  ctx.arc(x, y, radius, 0, 2 * Math.PI);
   ctx.stroke();
 }
 
@@ -17,10 +19,16 @@ function drawLine(x1, y1, x2, y2) {
   ctx.stroke();
 }
 
-drawCircle(canvasXCenter, 100);
-drawLine(canvasXCenter, 100 + 50, canvasXCenter, 250 - 50);
-drawLine(canvasXCenter, 100 + 50, canvasXCenter + 120, 250 - 50);
-drawLine(canvasXCenter, 100 + 50, canvasXCenter - 120, 250 - 50);
-drawCircle(canvasXCenter, 250);
-drawCircle(canvasXCenter + 120, 250);
-drawCircle(canvasXCenter - 120, 250);
+drawCircle(canvasXCenter, level * 1);
+drawLine(canvasXCenter, level * 1 + radius, canvasXCenter + margin, level * 2 - radius); //C
+drawLine(canvasXCenter, level * 1 + radius, canvasXCenter + margin * 3, level * 2 - radius); //D
+drawLine(canvasXCenter, level * 1 + radius, canvasXCenter - margin, level * 2 - radius); //B
+drawLine(canvasXCenter - margin , level * 2 + radius , canvasXCenter - margin, level * 3 - radius); //O
+drawLine(canvasXCenter, level * 1 + radius, canvasXCenter - margin * 3, level * 2 - radius); //A
+
+
+drawCircle(canvasXCenter - margin * 3 , level * 2) //A
+drawCircle(canvasXCenter - margin, level * 2); //B
+drawCircle(canvasXCenter + margin, level * 2); //C
+drawCircle(canvasXCenter + margin * 3, level * 2); //D
+drawCircle(canvasXCenter - margin, level * 3); //O
